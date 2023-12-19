db4f7662ee0df1ba137fa58fce76ec055a8b8cee
Nguyen Truong <xuantruong200101@gmai.com>
2023-12-19 21:32:15 +0700

    dev

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const compression = require('compression')
+const express = require('express')
+const morgan = require('morgan')
+const { default: helmet } = require('helmet')
+const app = express()
+
+// init middleware
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+
+// init db
+
+// init router
+
+app.get('/', (req, res) => {
+  return res.status(200).json({
+    message: 'Welcome',
+  })
+})
+
+// handling error
+
+
+
+module.exports = app
